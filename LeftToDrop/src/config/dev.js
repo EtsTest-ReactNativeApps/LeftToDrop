11daf2b30a363895c2235160420433fa97791951
krlargo <krlargo@ucdavis.edu>
2018-01-09 11:50:54 -0800

    Move original project to branch

---
+++ LeftToDrop/src/config/dev.js
@@ -0,0 +1,8 @@
+export default {
+  firebaseConfig: {
+    apiKey: 'AIzaSyA9QocrVseq9zvO_RI64HGDDf-HLuQYeXw',
+    authDomain: 'left-to-drop-9f551.firebaseapp.com',
+    databaseURL: 'https://left-to-drop-9f551.firebaseio.com',
+    storageBucket: 'left-to-drop-9f551.appspot.com'
+  }
+};
