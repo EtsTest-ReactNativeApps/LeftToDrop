ac310c73898cb2ac7ff2c83d2d34bbd4ceeb3339
krlargo <krlargo@ucdavis.edu>
2018-01-09 11:41:51 -0800

    Move to src directory

---
+++ LeftToDrop/src/components/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+import { StackNavigator } from 'react-navigation';
+
+import HomeScreen from './HomeScreen';
+/*import FavoritesScreen from './FavoritesScreen';
+import SeasonsScreen from './SeasonsScreen';
+import CategoriesScreen from './CategoriesScreen';
+import ItemsScreen from './ItemsScreen';
+import ItemScreen from './ItemScreen';*/
+
+var defaultNavOptions = () => {
+  return {
+    headerTitleStyle: {
+      fontFamily: 'Futura',
+      fontSize: 20,
+      fontStyle: 'italic'
+    },
+    headerStyle: {
+      backgroundColor: 'red'
+    },
+    headerTintColor: 'white'
+  };
+};
+
+const Navigator = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        title: 'Left To Drop'
+      }
+    }
+    /*Favorites: {
+		screen: FavoritesScreen,
+		navigationOptions: {
+			title: 'Favorites'
+		}
+	},
+	Seasons: {screen: SeasonsScreen},
+	Categories: {screen: CategoriesScreen},
+	Items: {screen: ItemsScreen},
+	Item: {screen: ItemScreen}*/
+  },
+  {
+    navigationOptions: defaultNavOptions()
+  }
+);
+
+class App extends Component {
+  render() {
+    return <Navigator />;
+  }
+}
+
+export default App;
