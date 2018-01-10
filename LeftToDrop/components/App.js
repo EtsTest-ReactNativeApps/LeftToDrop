138aa1f37a8adc115b2a2723759b91dbf1494998
krlargo <krlargo@ucdavis.edu>
2018-01-09 22:44:29 -0800

    Move to components folder

--- LeftToDrop/components/App.js
+++ LeftToDrop/components/App.js
@@ -1,13 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './HomeScreen';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello World</Text>
-      </View>
-    );
+    return <HomeScreen />;
   }
 }
 
