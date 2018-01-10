83ffe92ba4989dc4e6d847dbeaecdf607829a8c3
krlargo <krlargo@ucdavis.edu>
2018-01-09 23:30:06 -0800

    Convert to function subcomponent

--- LeftToDrop/components/HomeScreen.js
+++ LeftToDrop/components/HomeScreen.js
@@ -1,92 +1,16 @@
-import React, { Component } from 'react';
-import {
-  Button,
-  ListView,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View
-} from 'react-native';
+import React from 'react';
+import TableViewScreen from './TableViewScreen';
 
-class HomeScreen extends Component {
-  constructor() {
-    super();
-
-    const dataSource = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
-    });
-
-    destinations = [
-      {
-        text: 'Left To Drop',
-        screen: 'Seasons'
-      },
-      {
-        text: 'Already Dropped',
-        screen: 'Seasons'
-      },
-      {
-        text: 'Favorites',
-        screen: 'Favorites'
-      }
-    ];
-
-    this.state = {
-      dataSource: dataSource.cloneWithRows(destinations)
-    };
-  }
-
-  renderRow(rowData, sectionID, rowID) {
-    return (
-      <TouchableHighlight underlayColor="whitesmoke">
-        <View style={styles.cell}>
-          <Text style={styles.text}>{rowData.text}</Text>
-        </View>
-      </TouchableHighlight>
-    );
-  }
-
-  /*onPressRow(rowID) {
-    const { navigate } = this.props.navigation;
-    navigate(destinations[rowID].screen);
-  }*/
-
-  render() {
-    return (
-      <ListView
-        style={styles.container}
-        dataSource={this.state.dataSource}
-        renderRow={this.renderRow}
-        renderSeparator={(sectionID, rowID) => (
-          <View key={rowID} style={styles.separator} />
-        )}
-      />
-    );
-  }
-}
+const HomeScreen = () => {
+  return (
+    <TableViewScreen
+      destinations={[
+        { text: 'Left To Drop', screen: 'Seasons' },
+        { text: 'Previous Drops', screen: 'Seasons' },
+        { text: 'Favorites', screen: 'Favorites' }
+      ]}
+    />
+  );
+};
 
 export default HomeScreen;
-
-const styles = StyleSheet.create({
-  cell: {
-    alignItems: 'flex-start',
-    flex: 1,
-    height: 50,
-    justifyContent: 'center'
-  },
-  container: {
-    backgroundColor: 'white'
-  },
-  separator: {
-    backgroundColor: '#EEEEEE',
-    flex: 1,
-    height: 1
-  },
-  text: {
-    color: 'black',
-    fontFamily: 'Courier New',
-    fontSize: 15,
-    marginLeft: 15,
-    textAlign: 'left'
-  }
-});
