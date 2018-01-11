56f10fd83fd6ab2cf55037d34c2bdae9818f6b3b
krlargo <krlargo@ucdavis.edu>
2018-01-10 18:06:47 -0800

    Change to class-based component; handle props.navigation propagation

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TableViewScreen from './TableViewScreen';
 
-const HomeScreen = () => {
-  return (
-    <TableViewScreen
-      destinations={[
-        { text: 'Left To Drop', screen: 'Seasons' },
-        { text: 'Previous Drops', screen: 'Seasons' },
-        { text: 'Favorites', screen: 'Favorites' }
-      ]}
-    />
-  );
-};
+class HomeScreen extends Component {
+  render() {
+    return (
+      <TableViewScreen
+        destinations={[
+          { text: 'Left To Drop', screen: 'Seasons' },
+          { text: 'Previous Drops', screen: 'Seasons' },
+          { text: 'Favorites', screen: 'Seasons' }
+        ]}
+        navigation={this.props.navigation}
+      />
+    );
+  }
+}
 
 export default HomeScreen;
