d5b8c87850dd4efe1f0522be9714a85bc3bde3ba
krlargo <krlargo@ucdavis.edu>
2018-01-11 15:28:25 -0800

    Add 'id' cellData attribute

--- src/components/SeasonsScreen.js
+++ src/components/SeasonsScreen.js
@@ -21,6 +21,13 @@ class SeasonsScreen extends Component {
   }
 }
 
+/*
+  Get raw seasons data from Firebase, parse into appropriate cellData:
+  - id: used for propagated navigation
+  (e.g. clicking on SEASON_ID_1 will lead to SEASON_ID_1's categories)
+  - screen: the following screen to be routed to
+  - text: cell's text display
+*/
 const mapStateToProps = ({ seasons }) => {
   if (seasons) {
     const seasonNames = Object.keys(seasons);
@@ -28,8 +35,9 @@ const mapStateToProps = ({ seasons }) => {
     return {
       cellData: seasonNames.map(name => {
         return {
-          text: name,
-          screen: 'Categories'
+          id: name,
+          screen: 'Categories',
+          text: name
         };
       })
     };
