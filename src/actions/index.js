ab8705dc38e5342e59e2f35c419e62664f263e83
krlargo <krlargo@ucdavis.edu>
2018-01-11 23:41:20 -0800

    Move actions into individual files

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,4 @@
-import Firebase from 'firebase';
-import { firebaseConfig } from '../config/dev';
-
-export const FETCH_FAVORITES = 'FETCH_FAVORITES';
-export const FETCH_SEASONS = 'FETCH_SEASONS';
-export const FETCH_CATEGORIES = 'FETCH_CATEGORIES';
-export const FETCH_ITEMS = 'FETCH_ITEMS';
-export const FETCH_ITEM = 'FETCH_ITEM';
-
-const firebaseApp = Firebase.initializeApp(firebaseConfig);
-const rootRef = Firebase.database().ref();
-
-// Fetch Action Creators
-// From FavoritesRef
+import { FETCH_FAVORITES } from './types';
 export const fetchFavorites = userID => {
   return {
     type: FETCH_FAVORITES,
@@ -19,50 +6,8 @@ export const fetchFavorites = userID => {
   };
 };
 
-const seasonsRef = rootRef.child('seasons');
-export const fetchSeasons = () => dispatch => {
-  seasonsRef.on('value', snapshot => {
-    dispatch({
-      type: FETCH_SEASONS,
-      payload: snapshot.val()
-    });
-  });
-};
-
-const categoriesRef = rootRef.child('categories');
-export const fetchCategories = seasonID => dispatch => {
-  let categories = [];
-  // Get Season data
-  seasonsRef.child(seasonID).on('value', seasonSnap => {
-    const categoryIDs = Object.keys(seasonSnap.val());
-
-    for (let index in categoryIDs) {
-      let categoryID = categoryIDs[index];
-
-      categoriesRef.child(categoryID).once('value', categorySnap => {
-        categories.push(categorySnap.val());
-
-        // Dispatch after last Category only
-        if (index == categoryIDs.length - 1) {
-          dispatch({
-            type: FETCH_CATEGORIES,
-            payload: categories
-          });
-        }
-      });
-    }
-  });
-};
-
-// From CategoriesRef
-export const fetchItems = categoryID => {
-  return {
-    type: FETCH_ITEMS,
-    payload: []
-  };
-};
-
 // From ItemsRef
+import { FETCH_ITEM } from './types';
 export const fetchItem = itemID => {
   return {
     type: FETCH_ITEM,
@@ -72,6 +17,7 @@ export const fetchItem = itemID => {
 
 // Set Action Creators
 // To UsersRef
+import { SET_FAVORITE } from './types';
 export const setFavorite = (userID, itemID) => {
   return {
     type: SET_FAVORITE,
@@ -80,6 +26,7 @@ export const setFavorite = (userID, itemID) => {
 };
 
 // To ItemRef
+import { UPVOTE_ITEM } from './types';
 export const upvoteItem = (userID, itemID) => {
   return {
     type: UPVOTE_ITEM,
@@ -88,6 +35,7 @@ export const upvoteItem = (userID, itemID) => {
 };
 
 // To ItemRef
+import { DOWNVOTE_ITEM } from './types';
 export const downvoteItem = (userID, itemID) => {
   return {
     type: DOWNVOTE_ITEM,
