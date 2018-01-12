5c35a168e7f6ec766d92eaacdad6d948e5d1e391
krlargo <krlargo@ucdavis.edu>
2018-01-12 05:51:40 -0800

    Remove console.log

--- src/actions/fetch_user_action.js
+++ src/actions/fetch_user_action.js
@@ -3,7 +3,6 @@ import { FETCH_USER } from './types';
 
 export default (fetchUser = userID => dispatch => {
   usersRef.child(userID).on('value', snapshot => {
-    console.log('USERSNAPSHOTVAL: ', snapshot.val());
     dispatch({
       type: FETCH_USER,
       payload: snapshot.val()
