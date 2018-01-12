43686649540bafb644f466e7a80efa8338f0fd16
krlargo <krlargo@ucdavis.edu>
2018-01-11 23:34:21 -0800

    Convert snapshot.val() to array to match other action creators

--- src/actions/fetch_seasons_action.js
+++ src/actions/fetch_seasons_action.js
@@ -3,9 +3,14 @@ import { FETCH_SEASONS } from './types';
 
 export default (fetchSeasons = () => dispatch => {
   seasonsRef.on('value', snapshot => {
+    const seasonIDs = Object.keys(snapshot.val());
+    const seasons = seasonIDs.map(seasonID => {
+      return { [seasonID]: { name: seasonID } };
+    });
+
     dispatch({
       type: FETCH_SEASONS,
-      payload: snapshot.val()
+      payload: seasons
     });
   });
 });
