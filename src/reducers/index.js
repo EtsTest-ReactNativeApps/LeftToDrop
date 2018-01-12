aa7d614d32fe447210c9863babc6bd01c759f66c
krlargo <krlargo@ucdavis.edu>
2018-01-12 05:47:47 -0800

    Add ItemReducer and UserReducer to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,11 +5,13 @@ import seasonsReducer from './seasons_reducer';
 import categoriesReducer from './categories_reducer';
 import itemsReducer from './items_reducer';
 import itemReducer from './item_reducer';
+import userReducer from './user_reducer';
 
 export default (rootReducer = combineReducers({
   favorites: favoritesReducer,
   seasons: seasonsReducer,
   categories: categoriesReducer,
   items: itemsReducer,
-  item: itemReducer
+  item: itemReducer,
+  user: userReducer
 }));
