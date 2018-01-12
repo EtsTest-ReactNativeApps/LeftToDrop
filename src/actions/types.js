837fb49e35c4ad0300c5c043054b4a246faaeb48
krlargo <krlargo@ucdavis.edu>
2018-01-11 21:10:14 -0800

    Organize action creator types into its own file

---
+++ src/actions/types.js
@@ -0,0 +1,9 @@
+export const FETCH_FAVORITES = 'FETCH_FAVORITES';
+export const FETCH_SEASONS = 'FETCH_SEASONS';
+export const FETCH_CATEGORIES = 'FETCH_CATEGORIES';
+export const FETCH_ITEMS = 'FETCH_ITEMS';
+export const FETCH_ITEM = 'FETCH_ITEM';
+
+export const SET_FAVORITE = 'SET_FAVORITE';
+export const UPVOTE_ITEM = 'UPVOTE_ITEM';
+export const DOWNVOTE_ITEM = 'DOWNVOTE_ITEM';
