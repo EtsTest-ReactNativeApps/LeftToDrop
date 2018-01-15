442709ccd87b024f5b50cf9114879c1806d3fc34
krlargo <krlargo@ucdavis.edu>
2018-01-14 18:46:31 -0800

    Change Firebase observer to once()

--- src/actions/fetch_user_action.js
+++ src/actions/fetch_user_action.js
@@ -2,7 +2,7 @@ import { usersRef } from '../firebase/references';
 import { FETCH_USER } from './types';
 
 export default (fetchUser = userID => dispatch => {
-  usersRef.child(userID).on('value', snapshot => {
+  usersRef.child(userID).once('value', snapshot => {
     dispatch({
       type: FETCH_USER,
       payload: snapshot.val()
