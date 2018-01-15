427347147366bd17b2bb8573a026347739ac9b9e
krlargo <krlargo@ucdavis.edu>
2018-01-14 20:31:21 -0800

    Update fetchFavorites() to dispatch to separate ActionCreators

--- src/actions/fetch_favorites_action.js
+++ src/actions/fetch_favorites_action.js
@@ -1,50 +1,23 @@
 import { favoritesRef, itemsRef } from '../firebase/references';
-import { FETCH_FAVORITES } from './types';
+import { FETCH_FAVORITE_ITEM_IDS, FETCH_FAVORITE_ITEMS } from './types';
 
+// Single function that dispatches to separate Action Creators
 export const fetchFavorites = userID => dispatch => {
   favoritesRef.child(userID).on('value', snapshot => {
-    dispatch({
-      type: FETCH_FAVORITES,
-      payload: snapshot.val()
-    });
-  });
-};
-
-/*
-// Helper function that fetches an array of itemIDs
-// and passes it back to the calling Action Creator
-const getFavoriteItemIDs = (userID, callback) => {
-  favoritesRef.child(userID).on('value', snapshot => {
-    const itemIDs = Object.keys(snapshot.val());
-    callback(itemIDs);
-  });
-};
-
-// Passes an object array of favorite itemIDs to Redux
-// [ itemID: true, ...]
-export const fetchFavoriteItemIDs = userID => dispatch => {
-  getFavoriteItemIDs(userID, itemIDs => {
-    const itemIDsObject = itemIDs.map(id => {
-      return { [id]: true };
-    });
+    // Passes an object array of favorite itemIDs to Redux
     dispatch({
       type: FETCH_FAVORITE_ITEM_IDS,
-      payload: itemIDsObject
+      payload: snapshot.val()
     });
-  });
-};
 
-// Passes an object array of User's favorite Items to Redux
-// [ itemID: { ITEMFIELDKEY: ITEMFIELDVALUE, ...}, ...]
-export const fetchFavoriteItems = userID => dispatch => {
-  getFavoriteItemIDs(userID, itemIDs => {
-    var items = [];
+    // Passes an object array of User's favorite Items to Redux
+    const items = [];
+    const itemIDs = Object.keys(snapshot.val());
 
     itemIDs.forEach((itemID, index) => {
       // Gather corresponding Item data
       itemsRef.child(itemID).once('value', snapshot => {
-        item = { [snapshot.key]: snapshot.val() };
-        items.push(item);
+        items.push({ [snapshot.key]: snapshot.val() });
 
         // Dispatch after last Item only
         if (index == itemIDs.length - 1) {
@@ -56,4 +29,4 @@ export const fetchFavoriteItems = userID => dispatch => {
       });
     });
   });
-};*/
+};
