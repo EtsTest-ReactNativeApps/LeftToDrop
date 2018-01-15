37160b375f91824116c011ee7fdbf16ecb5e1707
krlargo <krlargo@ucdavis.edu>
2018-01-14 18:47:17 -0800

    Add dispatch to pass action to reducers

--- src/actions/item_action.js
+++ src/actions/item_action.js
@@ -1,14 +1,23 @@
 import { favoritesRef } from '../firebase/references';
+import { FAVORITE_ITEM, UNFAVORITE_ITEM } from './types';
 
 export const favoriteItem = (itemID, userID) => dispatch => {
   favoritesRef.child(userID).push({ [itemID]: true });
+  dispatch({
+    type: FAVORITE_ITEM,
+    payload: itemID
+  });
 };
 
 export const unfavoriteItem = (itemID, userID) => dispatch => {
-  return favoritesRef
+  favoritesRef
     .child(userID)
     .child(itemID)
     .remove();
+  dispatch({
+    type: UNFAVORITE_ITEM,
+    payload: itemID
+  });
 };
 
 /*export default downvoteItem = (itemID, userID) => dispatch => {
