5a8f381b70fae3dc825ad6e1c9cf81256081e369
krlargo <krlargo@ucdavis.edu>
2018-01-16 08:10:05 -0800

    Pass null by default to signify 'Loading' state

--- src/reducers/voted_items_reducer.js
+++ src/reducers/voted_items_reducer.js
@@ -3,21 +3,25 @@ import {
   FETCH_DOWNVOTED_ITEM_IDS
 } from '../actions/types';
 
-// Returns an array of upvotedItemIDs
+// Payloads are objects for constant-time access
+// - e.g. instead of state=[ID1, ID2, ...], use state={ID1: true, ID2: true, ...}
+// So we can check state['ID']
+
+// Returns an object array of upvotedItemIDs
 export const upvotedItemIDsReducer = (state = null, action) => {
   switch (action.type) {
     case FETCH_UPVOTED_ITEM_IDS:
-      return action.payload;
+      return action.payload || null;
     default:
       return state;
   }
 };
 
-// Returns an array of downvotedItemIDs
+// Returns an object array of downvotedItemIDs
 export const downvotedItemIDsReducer = (state = null, action) => {
   switch (action.type) {
     case FETCH_DOWNVOTED_ITEM_IDS:
-      return action.payload;
+      return action.payload || null;
     default:
       return state;
   }
