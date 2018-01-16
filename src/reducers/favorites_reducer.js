57774e108b429c8347ffe2b58f2e06c3964b2665
krlargo <krlargo@ucdavis.edu>
2018-01-16 08:05:39 -0800

    Pass null by default to signify 'Loading' state

--- src/reducers/favorites_reducer.js
+++ src/reducers/favorites_reducer.js
@@ -3,10 +3,15 @@ import {
   FETCH_FAVORITE_ITEMS
 } from '../actions/types';
 
+// Payloads are objects for constant-time access
+// - e.g. instead of state=[ID1, ID2, ...], use state={ID1: true, ID2: true, ...}
+// So we can check state['ID']
+
+// Returns an object array of favoriteItemIDs
 export const favoriteItemIDsReducer = (state = null, action) => {
   switch (action.type) {
     case FETCH_FAVORITE_ITEM_IDS:
-      return action.payload || {};
+      return action.payload || null;
     default:
       return state;
   }
@@ -15,7 +20,7 @@ export const favoriteItemIDsReducer = (state = null, action) => {
 export const favoriteItemsReducer = (state = null, action) => {
   switch (action.type) {
     case FETCH_FAVORITE_ITEMS:
-      return action.payload;
+      return action.payload || null;
     default:
       return state;
   }
