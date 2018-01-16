21f931692a7d7840433f8eb0c4e975be778a419d
krlargo <krlargo@ucdavis.edu>
2018-01-15 21:06:54 -0800

    Move styles to index.js

--- src/components/EmptyView.js
+++ src/components/EmptyView.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { defaultStyles } from '../styles';
 
 class EmptyView extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>{this.props.message}</Text>
+      <View style={defaultStyles.containerView}>
+        <Text
+          style={[defaultStyles.text, { marginTop: 10, textAlign: 'center' }]}
+        >
+          {this.props.message}
+        </Text>
       </View>
     );
   }
 }
 
 export default EmptyView;
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    flex: 1
-  },
-  text: {
-    color: 'black',
-    fontFamily: 'Courier New',
-    fontSize: 15,
-    marginTop: 15,
-    textAlign: 'center'
-  }
-});
