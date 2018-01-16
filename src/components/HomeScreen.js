ca5c663f45669b4f07f7a9aa6bf55933132b5314
krlargo <krlargo@ucdavis.edu>
2018-01-15 21:09:46 -0800

    Redirect 'Left To Drop' and 'Previous Drops' directly to Categories screen, load fetch currentSeason's ID

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import EmptyView from './EmptyView';
 import TableViewScreen from './TableViewScreen';
 import { fetchFavorites } from '../actions/fetch_favorites_action';
+import { fetchMetadata } from '../actions/fetch_metadata_action';
 
 class HomeScreen extends Component {
   // Parent Component owns the back button
@@ -11,25 +13,48 @@ class HomeScreen extends Component {
   });
 
   componentWillMount() {
+    this.props.fetchMetadata();
     this.props.fetchFavorites('krlargo');
   }
 
   render() {
-    return (
-      <TableViewScreen
-        staticCellData={[
-          { label: 'Left To Drop', screen: 'Seasons' },
-          { label: 'Previous Drops', screen: 'Seasons' },
-          { label: 'Favorites', screen: 'Favorites' }
-        ]}
-        navigation={this.props.navigation}
-      />
-    );
+    const { isLoading, metadata, navigation } = this.props;
+    if (!metadata) {
+      return <EmptyView message="Failed to load from database" />;
+    } else {
+      return (
+        <TableViewScreen
+          staticCellData={[
+            // Shows currentSeason's remainingItems
+            {
+              label: 'Left To Drop',
+              screen: 'Categories',
+              id: metadata.currentSeasonID
+            },
+            // Show's currentSeason's droppedItems
+            {
+              label: 'Previous Drops',
+              screen: 'Categories',
+              id: metadata.currentSeasonID
+            },
+            // Shows previousSeasons
+            { label: 'Seasons', screen: 'Seasons' },
+            // Shows favorites
+            { label: 'Favorites', screen: 'Favorites' }
+          ]}
+          navigation={navigation}
+        />
+      );
+    }
   }
 }
 
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators({ fetchFavorites }, dispatch);
+const mapStateToProps = ({ metadata }) => {
+  return { metadata };
 };
 
-export default connect(null, mapDispatchToProps)(HomeScreen);
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ fetchMetadata, fetchFavorites }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
