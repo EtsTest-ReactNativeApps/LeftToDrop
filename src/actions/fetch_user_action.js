a06f347e91f618e7608b96b94067936c1af533ac
krlargo <krlargo@ucdavis.edu>
2018-01-16 08:00:31 -0800

    Move FETCH_*VOTED_ITEM_IDS action dispatches to fetch_user_action.js

--- src/actions/fetch_user_action.js
+++ src/actions/fetch_user_action.js
@@ -1,11 +1,30 @@
 import { usersRef } from '../firebase/references';
-import { FETCH_USER } from './types';
+import {
+  FETCH_USER,
+  FETCH_UPVOTED_ITEM_IDS,
+  FETCH_DOWNVOTED_ITEM_IDS
+} from './types';
 
 export default (fetchUser = userID => dispatch => {
-  usersRef.child(userID).once('value', snapshot => {
+  usersRef.child(userID).on('value', snapshot => {
+    const userObject = snapshot.val() || {};
     dispatch({
       type: FETCH_USER,
-      payload: snapshot.val()
+      payload: userObject
+    });
+
+    // If snapshot[*votedItemIDs] doesn't exist, return empty object array
+
+    const upvotedItemIDsObjectArray = snapshot['upvotedItemIDs'] || {};
+    dispatch({
+      type: FETCH_UPVOTED_ITEM_IDS,
+      payload: upvotedItemIDsObjectArray
+    });
+
+    const downvotedItemIDsObjectArray = snapshot['downvotedItemIDs'] || {};
+    dispatch({
+      type: FETCH_DOWNVOTED_ITEM_IDS,
+      payload: downvotedItemIDsObjectArray
     });
   });
 });
