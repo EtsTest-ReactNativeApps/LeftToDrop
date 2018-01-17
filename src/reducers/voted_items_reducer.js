a36946551f3a2a01befb47e1fdb9d0225193a774
krlargo <krlargo@ucdavis.edu>
2018-01-16 16:10:17 -0800

    Remove unnecessary comment

--- src/reducers/voted_items_reducer.js
+++ src/reducers/voted_items_reducer.js
@@ -3,10 +3,6 @@ import {
   FETCH_DOWNVOTED_ITEM_IDS
 } from '../actions/types';
 
-// Payloads are objects for constant-time access
-// - e.g. instead of state=[ID1, ID2, ...], use state={ID1: true, ID2: true, ...}
-// So we can check state['ID']
-
 // Returns an object array of upvotedItemIDs
 export const upvotedItemIDsReducer = (state = null, action) => {
   switch (action.type) {
