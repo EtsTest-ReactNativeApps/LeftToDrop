5d544ccac2b249d643df115d228bfe66ac28652f
krlargo <krlargo@ucdavis.edu>
2018-01-16 17:04:18 -0800

    Minor changes

--- src/actions/fetch_favorites_action.js
+++ src/actions/fetch_favorites_action.js
@@ -1,11 +1,9 @@
 import { favoritesRef, itemsRef } from '../firebase/references';
 import { FETCH_FAVORITE_ITEM_IDS, FETCH_FAVORITE_ITEMS } from './types';
 
-// Single function that dispatches to separate Action Creators
 export const fetchFavorites = userID => dispatch => {
   favoritesRef.child(userID).on('value', snapshot => {
     // Passes an object array of favorite itemIDs
-    // If snapshot.val() deosn't exist, return empty array
     const favoriteItemIDsObjectArray = snapshot.val() || {};
     dispatch({
       type: FETCH_FAVORITE_ITEM_IDS,
@@ -18,7 +16,7 @@ export const fetchFavorites = userID => dispatch => {
 
     itemIDs.forEach((itemID, index) => {
       // Gather corresponding Item data
-      var promise = itemsRef.child(itemID).once('value', snapshot => {
+      const promise = itemsRef.child(itemID).once('value', snapshot => {
         items.push({ [snapshot.key]: snapshot.val() });
       });
       promises.push(promise);
