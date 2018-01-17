f2eb6f5a55b27e8a24273428fac84311f5cee871
krlargo <krlargo@ucdavis.edu>
2018-01-16 16:05:26 -0800

    Separate fetch*votedItem ActionCreators into separate file

--- src/actions/fetch_user_action.js
+++ src/actions/fetch_user_action.js
@@ -1,9 +1,5 @@
 import { usersRef } from '../firebase/references';
-import {
-  FETCH_USER,
-  FETCH_UPVOTED_ITEM_IDS,
-  FETCH_DOWNVOTED_ITEM_IDS
-} from './types';
+import { FETCH_USER } from './types';
 
 export default (fetchUser = userID => dispatch => {
   usersRef.child(userID).on('value', snapshot => {
@@ -11,19 +7,6 @@ export default (fetchUser = userID => dispatch => {
     dispatch({
       type: FETCH_USER,
       payload: userObject
-    });
-
-    // If snapshot[*votedItemIDs] doesn't exist, return empty object array
-    const upvotedItemIDsObjectArray = userObject['upvotedItemIDs'] || {};
-    dispatch({
-      type: FETCH_UPVOTED_ITEM_IDS,
-      payload: upvotedItemIDsObjectArray
-    });
-
-    const downvotedItemIDsObjectArray = userObject['downvotedItemIDs'] || {};
-    dispatch({
-      type: FETCH_DOWNVOTED_ITEM_IDS,
-      payload: downvotedItemIDsObjectArray
     });
   });
 });
