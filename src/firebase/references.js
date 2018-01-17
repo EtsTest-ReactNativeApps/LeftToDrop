73bbaed4e5911e17198bc09fc5bbf1cc44adcd93
krlargo <krlargo@ucdavis.edu>
2018-01-16 16:09:57 -0800

    Add up/downvoteItemsRef

--- src/firebase/references.js
+++ src/firebase/references.js
@@ -3,11 +3,13 @@ import { firebaseConfig } from '../config/dev';
 
 const firebaseApp = Firebase.initializeApp(firebaseConfig);
 
-const rootRef = Firebase.database().ref();
+export const rootRef = Firebase.database().ref();
 export const metadataRef = rootRef.child('_metadata');
-export const favoritesRef = rootRef.child('favorites');
 export const seasonsRef = rootRef.child('seasons');
 export const categoriesRef = rootRef.child('categories');
 export const itemsRef = rootRef.child('items');
 export const itemRef = rootRef.child('item');
 export const usersRef = rootRef.child('users');
+export const favoritesRef = rootRef.child('favorites');
+export const upvotedItemsRef = rootRef.child('upvotedItems');
+export const downvotedItemsRef = rootRef.child('downvotedItems');
