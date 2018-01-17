8fc574873e9a81a9ab955abad4751d8d7c560372
krlargo <krlargo@ucdavis.edu>
2018-01-16 16:59:55 -0800

    Refactor for cleaner code reuse

--- src/actions/set_item_action.js
+++ src/actions/set_item_action.js
@@ -1,46 +1,28 @@
-import {
-  rootRef,
-  itemsRef,
-  upvotedItemsRef,
-  downvotedItemsRef
-} from '../firebase/references';
+import { rootRef, itemsRef } from '../firebase/references';
 
-// 'value' will either be 'true' or 'null'
-
-export const toggleUpvoteItem = (itemID, userID, value) => dispatch => {
-  itemsRef.child(itemID + '/upvotes').transaction(upvoteCount => {
+const toggleItemVote = (itemID, userID, value, itemSubpath, rootPath) => {
+  itemsRef.child(itemID + '/' + itemSubpath).transaction(oldCount => {
     // Generate incremented/decremented value
-    const newCount = (upvoteCount || 0) + (value ? 1 : -1);
+    const newCount = (oldCount || 0) + (value ? 1 : -1);
 
     // Create new data object
-    let updatedUpvoteData = {};
-    updatedUpvoteData['upvotedItems/' + userID + '/' + itemID] = value;
-    updatedUpvoteData['items/' + itemID + '/upvotes'] = newCount;
+    let updatedVoteData = {};
+    updatedVoteData[rootPath + '/' + userID + '/' + itemID] = value;
+    updatedVoteData['items/' + itemID + '/' + itemSubpath] = newCount;
 
     // Execute deep-path update
-    rootRef.update(updatedUpvoteData, error => {
+    rootRef.update(updatedVoteData, error => {
       if (error) {
-        console.log('Failed to upvote item: ', error);
+        console.log('Failed to vote for item: ', error);
       }
     });
   });
 };
 
+export const toggleUpvoteItem = (itemID, userID, value) => dispatch => {
+  toggleItemVote(itemID, userID, value, 'upvotes', 'upvotedItems');
+};
+
 export const toggleDownvoteItem = (itemID, userID, value) => dispatch => {
-  itemsRef.child(itemID + '/downvotes').transaction(downvoteCount => {
-    // Generate incremented/decremented value
-    const newCount = (downvoteCount || 0) + (value ? 1 : -1);
-
-    // Create new data object
-    let updatedDownvoteData = {};
-    updatedDownvoteData['downvotedItems/' + userID + '/' + itemID] = value;
-    updatedDownvoteData['items/' + itemID + '/downvotes'] = newCount;
-
-    // Execute deep-path update
-    rootRef.update(updatedDownvoteData, error => {
-      if (error) {
-        console.log('Failed to downvote item: ', error);
-      }
-    });
-  });
+  toggleItemVote(itemID, userID, value, 'downvotes', 'downvotedItems');
 };
