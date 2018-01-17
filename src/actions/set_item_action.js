70c9dff68f6823cb73eb5aedce09b1c3f01241de
krlargo <krlargo@ucdavis.edu>
2018-01-16 16:06:48 -0800

    Finish up/downvote Firebase updating implementation

--- src/actions/set_item_action.js
+++ src/actions/set_item_action.js
@@ -1,15 +1,46 @@
-import { usersRef } from '../firebase/references';
+import {
+  rootRef,
+  itemsRef,
+  upvotedItemsRef,
+  downvotedItemsRef
+} from '../firebase/references';
 
 // 'value' will either be 'true' or 'null'
 
 export const toggleUpvoteItem = (itemID, userID, value) => dispatch => {
-  usersRef.child(userID + '/upvotedItemIDs').update({ [itemID]: value });
-  // Always remove from downvotedItemIDs just in case
-  usersRef.child(userID + '/downvotedItemIDs').update({ [itemID]: null });
+  itemsRef.child(itemID + '/upvotes').transaction(upvoteCount => {
+    // Generate incremented/decremented value
+    const newCount = (upvoteCount || 0) + (value ? 1 : -1);
+
+    // Create new data object
+    let updatedUpvoteData = {};
+    updatedUpvoteData['upvotedItems/' + userID + '/' + itemID] = value;
+    updatedUpvoteData['items/' + itemID + '/upvotes'] = newCount;
+
+    // Execute deep-path update
+    rootRef.update(updatedUpvoteData, error => {
+      if (error) {
+        console.log('Failed to upvote item: ', error);
+      }
+    });
+  });
 };
 
 export const toggleDownvoteItem = (itemID, userID, value) => dispatch => {
-  usersRef.child(userID + '/downvotedItemIDs').update({ [itemID]: value });
-  // Always remove from upvotedItemIDs just in case
-  usersRef.child(userID + '/upvotedItemIDs').update({ [itemID]: null });
+  itemsRef.child(itemID + '/downvotes').transaction(downvoteCount => {
+    // Generate incremented/decremented value
+    const newCount = (downvoteCount || 0) + (value ? 1 : -1);
+
+    // Create new data object
+    let updatedDownvoteData = {};
+    updatedDownvoteData['downvotedItems/' + userID + '/' + itemID] = value;
+    updatedDownvoteData['items/' + itemID + '/downvotes'] = newCount;
+
+    // Execute deep-path update
+    rootRef.update(updatedDownvoteData, error => {
+      if (error) {
+        console.log('Failed to downvote item: ', error);
+      }
+    });
+  });
 };
