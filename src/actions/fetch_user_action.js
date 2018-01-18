cbd8e22e259be0e0fe6ab315c5e8b82b425ee97b
krlargo <krlargo@ucdavis.edu>
2018-01-17 22:08:47 -0800

    Chain fetch user votes/favorites actions to fetchUser action

--- src/actions/fetch_user_action.js
+++ src/actions/fetch_user_action.js
@@ -1,12 +1,30 @@
+import { bindActionCreators } from 'redux';
 import { usersRef } from '../firebase/references';
+import { fetchFavorites } from './fetch_favorites_action';
+import {
+  fetchUpvotedItemIDs,
+  fetchDownvotedItemIDs
+} from './fetch_voted_items_action';
 import { FETCH_USER } from './types';
 
 export default (fetchUser = userID => dispatch => {
   usersRef.child(userID).on('value', snapshot => {
     const userObject = snapshot.val() || {};
+    userObject['id'] = snapshot.key;
+
     dispatch({
       type: FETCH_USER,
       payload: userObject
     });
   });
+
+  // Bind fetch actions to dispatch
+  const chainedActions = bindActionCreators(
+    { fetchFavorites, fetchUpvotedItemIDs, fetchDownvotedItemIDs },
+    dispatch
+  );
+  // Call each fetch action
+  for (action in chainedActions) {
+    chainedActions[action](userID);
+  }
 });
