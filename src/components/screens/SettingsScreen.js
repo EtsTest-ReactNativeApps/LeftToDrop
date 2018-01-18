d7b2c8b302626a06db61f6b1af2f13795d9eb8a5
krlargo <krlargo@ucdavis.edu>
2018-01-17 22:12:03 -0800

    Start SettingsScreen

---
+++ src/components/screens/SettingsScreen.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from 'react-native';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import SeparatorView from '../subcomponents/SeparatorView';
+
+class StaticRow extends Component {
+  render() {
+    const { label, value, onPressRow, labelStyle } = this.props;
+    return (
+      <TouchableHighlight underlayColor="whitesmoke" onPress={onPressRow}>
+        <View style={styles.cell}>
+          <Text style={[defaultStyles.text, labelStyle]}>{label}</Text>
+          <Text style={[defaultStyles.text, valueStyle]}>{value}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+}
+
+class SettingsScreen extends Component {
+  changeUsernamePopup() {
+    console.log('Present change username popup');
+  }
+
+  changeEmailPopup() {
+    console.log('Present change email popup');
+  }
+
+  changePasswordPopup() {
+    console.log('Present change password popup');
+  }
+
+  aboutPopup() {
+    console.log('Present About popup');
+  }
+
+  deleteAccountPopup() {
+    console.log('Present delete account popup');
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <StaticRow
+          label="Username"
+          value={this.props.user.username}
+          onPressRow={this.changeUsernamePopup}
+        />
+        <SeparatorView />
+        <StaticRow
+          label="Email"
+          value={this.props.user.email}
+          onPressRow={this.changeUsernamePopup}
+        />
+        <SeparatorView />
+        <StaticRow
+          label="Password"
+          value="*******"
+          onPressRow={this.changePasswordPopup}
+        />
+        <SeparatorView />
+        <View style={{ height: 25 }} /> {/* Spacer */}
+        <SeparatorView />
+        <StaticRow label="About" onPressRow={this.navigateToAbout} />
+        <SeparatorView />
+        <View style={{ height: 25 }} /> {/* Spacer */}
+        <SeparatorView />
+        <StaticRow
+          label="Delete Account"
+          onPressRow={this.deleteAccountPopup}
+          labelStyle={styles.deleteLabel}
+        />
+      </ScrollView>
+    );
+  }
+}
+
+mapStateToProps = ({ user }) => {
+  return { user };
+};
+
+mapDispatchToProps => dispatch => {
+  return bindActionCreators(
+    { setUsername, setPassword, setEmail, deleteAccount },
+    dispatch
+  );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SettingsScreen);
+
+const styles = StyleSheet.create({
+  deleteLabel: {
+    color: 'red'
+  }
+});
