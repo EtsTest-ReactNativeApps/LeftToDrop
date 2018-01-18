971140873499aa3ab76530013e638290b08b983d
krlargo <krlargo@ucdavis.edu>
2018-01-17 23:13:22 -0800

    Setup styles, create dynamic SeparatorView

--- src/components/screens/SettingsScreen.js
+++ src/components/screens/SettingsScreen.js
@@ -8,22 +8,29 @@ import {
 } from 'react-native';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import SeparatorView from '../subcomponents/SeparatorView';
-import { defaultStyles, settingsScreenStyles as styles } from '../../styles';
+import {
+  defaultStyles,
+  settingsScreenStyles as styles,
+  listViewStyles
+} from '../../styles';
 
 class StaticRow extends Component {
   render() {
     const { label, value, onPressRow, labelStyle } = this.props;
     return (
       <TouchableHighlight underlayColor="whitesmoke" onPress={onPressRow}>
-        <View style={styles.cell}>
+        <View style={listViewStyles.cell}>
           <Text style={[defaultStyles.text, labelStyle]}>{label}</Text>
-          <Text style={[defaultStyles.text, valueStyle]}>{value}</Text>
+          <Text style={[defaultStyles.text, styles.valueStyle]}>{value}</Text>
         </View>
       </TouchableHighlight>
     );
   }
 }
+
+const SpacerView = () => {
+  return <View style={listViewStyles.spacer} />;
+};
 
 class SettingsScreen extends Component {
   changeUsernamePopup() {
@@ -48,31 +55,25 @@ class SettingsScreen extends Component {
 
   render() {
     return (
-      <ScrollView>
+      <ScrollView style={defaultStyles.containerView}>
         <StaticRow
           label="Username"
           value={this.props.user.username}
           onPressRow={this.changeUsernamePopup}
         />
-        <SeparatorView />
         <StaticRow
           label="Email"
           value={this.props.user.email}
           onPressRow={this.changeUsernamePopup}
         />
-        <SeparatorView />
         <StaticRow
           label="Password"
           value="*******"
           onPressRow={this.changePasswordPopup}
         />
-        <SeparatorView />
-        <View style={{ height: 25 }} /> {/* Spacer */}
-        <SeparatorView />
+        <SpacerView />
         <StaticRow label="About" onPressRow={this.navigateToAbout} />
-        <SeparatorView />
-        <View style={{ height: 25 }} /> {/* Spacer */}
-        <SeparatorView />
+        <SpacerView />
         <StaticRow
           label="Delete Account"
           onPressRow={this.deleteAccountPopup}
