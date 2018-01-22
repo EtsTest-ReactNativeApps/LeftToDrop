1e0753c930c4e7aba09a7034d3eef403372603eb
krlargo <krlargo@ucdavis.edu>
2018-01-22 02:48:00 -0800

    Change buttonStylesCreator import path

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,10 +1,7 @@
 export { defaults, defaultStyles } from './defaults';
 export { defaultNavigationOptions } from './stack_navigator_styles';
 export { listViewStyles } from './list_view_styles';
-export {
-  itemScreenStyles,
-  itemButtonStylesCreator,
-  ratingBarStylesCreator
-} from './item_screen_styles';
+export { itemScreenStyles, ratingBarStylesCreator } from './item_screen_styles';
+export { buttonStylesCreator } from './button_styles';
 export { gridViewStyles } from './grid_view_styles';
 export { modalViewStyles } from './modal_view_styles';
