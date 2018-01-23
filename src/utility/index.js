838de0b1536ca708c6da975a5ce24eb791f4dc47
krlargo <krlargo@ucdavis.edu>
2018-01-23 02:29:45 -0800

    Add isValidEmail()

--- src/utility/index.js
+++ src/utility/index.js
@@ -1,3 +1,21 @@
-export const capitalize = string => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+export const isAlphaNumeric = str => {
+  var code, i, len;
+
+  for (i = 0, len = str.length; i < len; i++) {
+    code = str.charCodeAt(i);
+    if (
+      !(code > 47 && code < 58) && // numeric (0-9)
+      !(code > 64 && code < 91) && // upper alpha (A-Z)
+      !(code > 96 && code < 123)
+    ) {
+      // lower alpha (a-z)
+      return false;
+    }
+  }
+  return true;
+};
+
+export const isValidEmail = email => {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email.toLowerCase());
 };
