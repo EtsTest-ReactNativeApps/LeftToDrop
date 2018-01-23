eb5c229bd9a68f58c50bcbf9846d3b068cdb60b5
krlargo <krlargo@ucdavis.edu>
2018-01-22 18:42:32 -0800

    Setup styling for modals without input fields

--- src/components/subcomponents/ModalView.js
+++ src/components/subcomponents/ModalView.js
@@ -8,12 +8,18 @@ import {
   View
 } from 'react-native';
 import ItemButton from '../subcomponents/ItemButton';
-import { defaultStyles, modalViewStyles as styles } from '../../styles';
+import {
+  defaults,
+  defaultStyles,
+  modalViewStyles as styles
+} from '../../styles';
 
 class ModalView extends Component {
   render() {
     const { closeModal, contents, visibility } = this.props;
     const { description, value, cancelLabel, submitLabel } = contents;
+
+    console.log('CONTENTS: ' + JSON.stringify(contents));
 
     return (
       <Modal
@@ -26,53 +32,59 @@ class ModalView extends Component {
           style={styles.modalContainerView}
           onPress={closeModal}
         >
-          <View>
-            <View style={{ flex: 1 }} />
-            <TouchableHighlight style={styles.modalView} onPress={null}>
-              <View style={styles.content}>
-                <View style={[styles.description, { flex: value ? 1 : 2 }]}>
-                  <Text style={[defaultStyles.text, { textAlign: 'center' }]}>
-                    {description}
-                  </Text>
-                </View>
+          <TouchableHighlight style={styles.modalView} onPress={null}>
+            <View style={styles.content}>
+              <View
+                style={[
+                  styles.description,
+                  (() => {
+                    // Double desription height if there are no textFields
+                    if (!value) {
+                      return { height: defaults.cellContentHeight * 2 };
+                    }
+                  })()
+                ]}
+              >
+                <Text style={[defaultStyles.text, { textAlign: 'center' }]}>
+                  {description}
+                </Text>
+              </View>
+              {(() => {
+                if (value) {
+                  return (
+                    <View style={styles.inputRow}>
+                      <TextInput
+                        style={[defaultStyles.text, styles.textInput]}
+                        value={value}
+                      />
+                    </View>
+                  );
+                }
+              })()}
+              <View style={styles.buttonRow}>
                 {(() => {
-                  if (value) {
+                  if (cancelLabel) {
                     return (
-                      <View style={styles.textField /* TextInput row */}>
-                        <TextInput
-                          style={[defaultStyles.text, styles.textInput]}
-                          value={value}
-                        />
-                      </View>
+                      <ItemButton
+                        onPress={closeModal}
+                        label={cancelLabel}
+                        color={'black'}
+                        marginLeft={0}
+                        marginRight={5}
+                      />
                     );
                   }
                 })()}
-                <View style={styles.buttonRow /* Button row */}>
-                  {(() => {
-                    if (cancelLabel) {
-                      return (
-                        <ItemButton
-                          onPress={closeModal}
-                          label={cancelLabel}
-                          color={'black'}
-                          marginLeft={0}
-                          marginRight={5}
-                        />
-                      );
-                    }
-                  })()}
-                  <ItemButton
-                    onPress={closeModal}
-                    label={submitLabel}
-                    color={'red'}
-                    marginLeft={cancelLabel ? 5 : 0}
-                    marginRight={0}
-                  />
-                </View>
+                <ItemButton
+                  onPress={closeModal}
+                  label={submitLabel}
+                  color={'red'}
+                  marginLeft={cancelLabel ? 5 : 0}
+                  marginRight={0}
+                />
               </View>
-            </TouchableHighlight>
-            <View style={{ flex: 2 }} />
-          </View>
+            </View>
+          </TouchableHighlight>
         </TouchableHighlight>
       </Modal>
     );
--- src/styles/modal_view_styles.js
+++ src/styles/modal_view_styles.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
 import { defaults, defaultStyles } from './defaults';
 
 export const modalViewStyles = StyleSheet.create({
@@ -6,33 +6,43 @@ export const modalViewStyles = StyleSheet.create({
   modalContainerView: {
     alignItems: 'center',
     backgroundColor: '#00000080',
-    justifyContent: 'center'
+    flex: 1
   },
   // Actual popup window
   modalView: {
-    aspectRatio: 16 / 9,
+    //aspectRatio: 16 / 9,
     backgroundColor: defaults.contentBackgroundColor,
     borderRadius: 10,
     padding: 10,
+    top: Dimensions.get('window').height / 4,
     width: '90%'
   },
   // Modal's Text, TextInput, and Button positioning
   content: {
     alignItems: 'center',
-    flex: 1,
     flexDirection: 'column',
-    justifyContent: 'space-between' //'center'
+    justifyContent: 'space-between', //'center'
+    padding: 2.5
+  },
+  contentRow: {
+    alignItems: 'center',
+    borderColor: 'blue',
+    borderWidth: 1,
+    height: defaults.cellContentHeight,
+    margin: 2.5,
+    width: '100%'
   },
   // Field styling; top row of modal
   description: {
     alignItems: 'center',
-    flex: 1,
+    height: defaults.cellContentHeight,
     justifyContent: 'center',
     marginBottom: 2.5,
     width: '100%'
   },
   // View containing TextInput
-  textField: {
+  inputRow: {
+    alignItems: 'center',
     height: defaults.cellContentHeight,
     justifyContent: 'center',
     marginVertical: 5,
@@ -42,11 +52,13 @@ export const modalViewStyles = StyleSheet.create({
   textInput: {
     borderWidth: 1,
     height: '100%',
-    paddingHorizontal: 5
+    paddingHorizontal: 5,
+    width: '100%'
   },
   // Row containing horizontally aligned buttons; bottom row of modal
   buttonRow: {
     flexDirection: 'row',
+    height: defaults.cellContentHeight,
     justifyContent: 'center',
     marginTop: 2.5
   }
