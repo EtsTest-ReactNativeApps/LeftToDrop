65ccd0aa91bb6818bbce042a7e3bd4c3000bdf2c
krlargo <krlargo@ucdavis.edu>
2018-01-23 17:07:37 -0800

    Add callback to handle errors, add logout action creator, add joinDate

--- src/actions/set_user_action.js
+++ src/actions/set_user_action.js
@@ -1,40 +1,66 @@
 import firebase from 'firebase';
 import { bindActionCreators } from 'redux';
 import { fetchUser } from './';
+import { LOGOUT_USER } from './types';
 import { usersRef } from '../firebase/references';
 
-export const firebaseLogin = (email, password) => dispatch => {
+export const firebaseLogin = (email, password, callback) => dispatch => {
   firebase
     .auth()
     .signInWithEmailAndPassword(email, password)
     .then(user => {
       const chainedActions = bindActionCreators({ fetchUser }, dispatch);
-      chainedActions.fetchUser(user.uid);
-      return null;
+      chainedActions.fetchUser();
+      callback(null);
     })
     .catch(error => {
       const { code, message } = error;
-      return message;
+      callback(message);
     });
 };
 
-export const firebaseSignup = (email, password) => dispatch => {
+export const firebaseLogout = () => dispatch => {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      dispatch({
+        type: LOGOUT_USER
+      });
+    });
+};
+
+export const firebaseSignup = (
+  email,
+  username,
+  password,
+  callback
+) => dispatch => {
   firebase
     .auth()
     .createUserWithEmailAndPassword(email, password)
     .then(user => {
-      usersRef.set({
-        [user.uid]: {
-          email: user.email,
-          username: user.username
-        }
-      });
-      const chainedActions = bindActionCreators({ fetchUser }, dispatch);
-      chainedActions.fetchUser(user.uid);
-      return { signupError: null };
+      const date = new Date();
+      const joinDate =
+        date.getMonth() + 1 + '/' + date.getDate() + '/' + date.getFullYear();
+
+      // Add new user to database
+      usersRef
+        .set({
+          [user.uid]: {
+            email: user.email,
+            username: user.username,
+            joinDate
+          }
+        })
+        .then(() => {
+          const chainedActions = bindActionCreators({ fetchUser }, dispatch);
+          chainedActions.fetchUser(user.uid);
+          callback(null);
+        });
     })
     .catch(error => {
       const { code, message } = error;
-      return { signupError: message };
+      callback(message);
     });
 };
