df73704e42488bec29d01e04c7064f9442710d1a
krlargo <krlargo@ucdavis.edu>
2018-01-24 00:50:04 -0800

    Change logged out user to null instead of {}

--- src/reducers/user_reducer.js
+++ src/reducers/user_reducer.js
@@ -3,9 +3,9 @@ import { FETCH_USER, LOGOUT_USER } from '../actions/types';
 export default (userReducer = (state = null, action) => {
   switch (action.type) {
     case FETCH_USER:
-      return action.payload || {};
+      return action.payload;
     case LOGOUT_USER:
-      return {};
+      return null;
     default:
       return state;
   }
