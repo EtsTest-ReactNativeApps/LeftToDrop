ace134353ec58a4e1239bf9546e5229f6ed1e7a3
krlargo <krlargo@ucdavis.edu>
2018-01-24 00:01:08 -0800

    Add usernamesRef

--- src/firebase/references.js
+++ src/firebase/references.js
@@ -29,6 +29,7 @@ export const categoriesRef = rootRef.child('categories');
 export const itemsRef = rootRef.child('items');
 export const itemRef = rootRef.child('item');
 export const usersRef = rootRef.child('users');
+export const usernamesRef = rootRef.child('usernames');
 export const favoritesRef = rootRef.child('favorites');
 export const upvotedItemsRef = rootRef.child('upvotedItems');
 export const downvotedItemsRef = rootRef.child('downvotedItems');
