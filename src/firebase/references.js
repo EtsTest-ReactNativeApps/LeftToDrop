b54cdcce13ad014a64ba82bfdfbbebb640b6bae2
krlargo <krlargo@ucdavis.edu>
2018-01-25 21:33:49 -0800

    Remove up/downvotedItemsRef, replace with user/itemVotesRef

--- src/firebase/references.js
+++ src/firebase/references.js
@@ -1,25 +1,6 @@
 import Firebase from 'firebase';
 import { firebaseConfig } from '../config/dev';
 
-/*/,,,
-import functions from 'firebase-functions';
-import admin from 'firebase-admin';
-admin.initializeApp(functions.config().firebase);
-
-export const updateItemVoteCounts = functions.database
-  .ref('items/{itemID}/upvotes')
-  .onWrite(event => {
-    console.log('ON_WRITE CALLED');
-    return itemsRef.child(itemID + '/' + itemSubpath).transaction(current => {
-      if (event.data.exists() && !event.data.previous.exists()) {
-        return (current || 0) + 1;
-      } else if (!event.data.exists() && event.data.previous.exists()) {
-        return (current || 0) - 1;
-      }
-    });
-  });
-//*/
-
 const firebaseApp = Firebase.initializeApp(firebaseConfig);
 
 export const rootRef = Firebase.database().ref();
@@ -30,6 +11,6 @@ export const itemsRef = rootRef.child('items');
 export const itemRef = rootRef.child('item');
 export const usersRef = rootRef.child('users');
 export const usernamesRef = rootRef.child('usernames');
-export const favoritesRef = rootRef.child('favorites');
-export const upvotedItemsRef = rootRef.child('upvotedItems');
-export const downvotedItemsRef = rootRef.child('downvotedItems');
+export const favoritesRef = rootRef.child('userFavorites');
+export const userVotesRef = rootRef.child('userVotes');
+export const itemVotesRef = rootRef.child('itemVotes');
